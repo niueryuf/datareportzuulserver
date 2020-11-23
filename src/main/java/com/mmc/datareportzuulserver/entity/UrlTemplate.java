f492d10475c0b5803d9e7a042012847087eb2269
陈邵勇 <sy13141979192@163.com>
2020-11-23 17:19:14 +0800

    v2.0

---
+++ src/main/java/com/mmc/datareportzuulserver/entity/UrlTemplate.java
@@ -0,0 +1,10 @@
+package com.mmc.datareportzuulserver.entity;
+
+/**
+ * @author csy
+ * @version 1.0
+ * @date 2020/9/15 21:16
+ */
+public interface UrlTemplate {
+    String[] EXTERNALURLS = {"/usercentersystemserver/external/groundLogin" , "/usercentersystemserver/external/bindDeviceAppUser","/usercentersystemserver/external/logout"};
+}
--- src/main/java/com/mmc/datareportzuulserver/filter/PreZuulFilter.java
+++ src/main/java/com/mmc/datareportzuulserver/filter/PreZuulFilter.java
@@ -1,5 +1,6 @@
 package com.mmc.datareportzuulserver.filter;
 
+import com.mmc.datareportzuulserver.entity.UrlTemplate;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -32,7 +37,12 @@ public class PreZuulFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        RequestContext rc = RequestContext.getCurrentContext();
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        for (String externalurl : UrlTemplate.EXTERNALURLS) {
+            if (externalurl.equals(request.getRequestURI())){
+                return false;
+            }
+        }
         return true;
     }
 
@@ -46,6 +56,7 @@ public class PreZuulFilter extends ZuulFilter {
             rc.setResponseBody(body);
             rc.setSendZuulResponse(false);
         }
+        System.out.println("====> ok !");
         return null;
     }
 
@@ -53,13 +64,28 @@ public class PreZuulFilter extends ZuulFilter {
     public boolean checkToken(HttpServletRequest request) {
         String token = request.getHeader("token");
         if (!StringUtils.isEmpty(token)) {
-            String key = token ;
-            String flyingsessionid = (String)stringRedisTemplate.opsForValue().get(key);
-            if (flyingsessionid != null) {
-                stringRedisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+            String username = (String) stringRedisTemplate.opsForValue().get(token);
+            System.out.println( "username:" + username);
+            if (username != null) {
+                stringRedisTemplate.expire(token, expireTime, TimeUnit.SECONDS);
+                setRequestParameter(request, username);
                 return true;
             }
         }
         return false;
+    }
+
+    public void setRequestParameter(HttpServletRequest request , String username) {
+        RequestContext rc = RequestContext.getCurrentContext();
+        request.getParameterMap();
+        Map<String, List<String>> requestQueryParams = rc.getRequestQueryParams();
+
+        if (requestQueryParams==null) {
+            requestQueryParams=new HashMap<>();
+        }
+        ArrayList<String> usernameList = new ArrayList<>();
+        usernameList.add(username);
+        requestQueryParams.put("current_username", usernameList);
+        rc.setRequestQueryParams(requestQueryParams);
     }
 }
